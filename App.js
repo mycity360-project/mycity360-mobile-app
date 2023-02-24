38ce8fadc1fe4d2965f737b3b94b616f8f7e2c75
vibhor-ts <vibhor.malik@deliveryhero.in>
2023-02-24 23:01:43 +0530

    Adding icon and fix simple code

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+import {Signup} from './src/pages/signup';
+
+function App() {
+  return (
+    <View>
+      <Signup />
+    </View>
+  );
+}
+
+export default App;
---
+++ src/pages/signup.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export function Signup() {
+  return <Text>This is New Signup Page</Text>;
+}
