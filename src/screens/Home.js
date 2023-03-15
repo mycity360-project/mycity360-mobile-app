850dd0f751f88ed5af9a663b16a82af026304e07
anurag <anuragchachan97@gmail.com>
2023-03-15 18:33:30 +0530

    Home Screen horizontal category list done

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -9,8 +9,10 @@ import {
   Image,
   TouchableOpacity,
   FlatList,
+  SectionList,
+  ScrollView,
 } from 'react-native';
-import {React, useContext} from 'react';
+import {React, useCallback, useContext} from 'react';
 import {AuthContext} from '../context/AuthContext';
 import CustomButton from '../shared/components/CustomButton';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
@@ -18,11 +20,32 @@ import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 export default function Home() {
   const {logout} = useContext(AuthContext);
   const data = [
-    {id: '1', title: 'Bike', imgUri: '../assets/icons/bike.png'},
-    {id: '2', title: 'Electronics', imgUri: '../assets/icons/bike.png'},
-    {id: '3', title: 'Mobile', imgUri: '../assets/icons/bike.png'},
-    {id: '4', title: 'Properties', imgUri: '../assets/icons/bike.png'},
-    {id: '5', title: 'Jobs', imgUri: '../assets/icons/bike.png'},
+    {key: 1, title: 'Bike', imgUri: require('../assets/icons/bike.png')},
+    {
+      key: 2,
+      title: 'Electronics',
+      imgUri: require('../assets/icons/electronics.png'),
+    },
+    {key: 3, title: 'Mobile', imgUri: require('../assets/icons/mobile.png')},
+    {
+      key: 4,
+      title: 'Properties',
+      imgUri: require('../assets/icons/properties.png'),
+    },
+    {key: 5, title: 'Jobs', imgUri: require('../assets/icons/jobs.png')},
+    {key: 6, title: 'Jobs', imgUri: require('../assets/icons/jobs.png')},
+    {key: 7, title: 'Jobs', imgUri: require('../assets/icons/jobs.png')},
+    {
+      key: 8,
+      title: 'Electronics',
+      imgUri: require('../assets/icons/electronics.png'),
+    },
+    {key: 9, title: 'Mobile', imgUri: require('../assets/icons/mobile.png')},
+    {
+      key: 10,
+      title: 'Properties',
+      imgUri: require('../assets/icons/properties.png'),
+    },
   ];
 
   return (
@@ -68,18 +91,34 @@ export default function Home() {
             </View>
           </View>
           <View style={styles.categoryContainer}>
-            <TouchableOpacity
-              style={{
-                marginLeft: 5,
-                alignItems: 'center',
-              }}>
+            <TouchableOpacity style={styles.sellContainer}>
               <Image
                 source={require('../assets/icons/Sell.png')}
-                style={{width: 40, height: 40}}
+                style={{width: 30, height: 30}}
               />
-              <Text style={{fontSize: 16, fontWeight: 500}}>Sell</Text>
+              <Text style={{fontSize: 16, fontWeight: 500, color: '#111'}}>
+                Sell
+              </Text>
             </TouchableOpacity>
+
+            <ScrollView
+              horizontal
+              decelerationRate={'normal'}
+              showsHorizontalScrollIndicator={false}>
+              {data.map(item => {
+                return (
+                  <View key={item.key} style={styles.categoryList}>
+                    <Image source={item.imgUri} style={styles.categoryImg} />
+                    <Text
+                      style={{fontSize: 16, fontWeight: 500, color: '#111'}}>
+                      {item.title}
+                    </Text>
+                  </View>
+                );
+              })}
+            </ScrollView>
           </View>
+
           <View style={styles.featuredAds}></View>
           <View style={styles.btmTab}></View>
         </View>
@@ -130,7 +169,21 @@ const styles = StyleSheet.create({
     // backgroundColor: '#456490',
     padding: 5,
     gap: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
+  sellContainer: {
+    marginLeft: '1%',
+    alignItems: 'center',
+    // backgroundColor: '#555',
+    width: '12%',
+  },
+  categoryList: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 14,
+  },
+  categoryImg: {width: 30, height: 30},
   featuredAds: {flex: 3},
   btmTab: {flex: 1},
 });
