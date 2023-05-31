c5f205072cd47e395b1eb76ef36cd41c14d9bca0
anurag <anuragchachan97@gmail.com>
2023-05-31 15:54:47 +0530

    no-internet

--- src/navigation/AppNav.js
+++ src/navigation/AppNav.js
@@ -3,10 +3,42 @@ import {NavigationContainer} from '@react-navigation/native';
 import AuthStack from './AuthStack';
 import AppStack from './AppStack';
 import {AuthContext} from '../context/AuthContext';
-import {ActivityIndicator, View} from 'react-native';
-import InternetConnection from '../shared/components/InternetConnection';
+import {ActivityIndicator, View, Image, Text} from 'react-native';
+import NetInfo from '@react-native-community/netinfo';
+
 export default function AppNav() {
   const {isLoading, userToken = null} = useContext(AuthContext);
+  const [isConnected, setIsConnected] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      const hasInternetConnection = state.isConnected;
+
+      if (!hasInternetConnection && isConnected) {
+        setIsConnected(false);
+      } else if (hasInternetConnection && !isConnected) {
+        setIsConnected(true);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [isConnected]);
+
+  if (!isConnected) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Image
+          source={require('../assets/icons/no-internet.png')}
+          style={{width: 50, height: 50}}
+        />
+        <Text style={{color: '#111', fontSize: 14, fontWeight: 500}}>
+          No Internet. Please check your connection.
+        </Text>
+      </View>
+    );
+  }
 
   if (isLoading) {
     return (
@@ -20,7 +52,6 @@ export default function AppNav() {
   if (userToken === null || userToken === '') {
     return (
       <NavigationContainer>
-        <InternetConnection />
         <AuthStack />
       </NavigationContainer>
     );
@@ -28,7 +59,6 @@ export default function AppNav() {
 
   return (
     <NavigationContainer>
-      <InternetConnection />
       <AppStack />
     </NavigationContainer>
   );
