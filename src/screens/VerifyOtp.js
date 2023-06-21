c260fb2b0dbbef38d7773e11facc70f03b663b23
vibhor-ts <vibhor.malik@deliveryhero.in>
2023-06-21 22:45:39 +0530

    Fix verifyOtp Screen

--- src/screens/VerifyOtp.js
+++ src/screens/VerifyOtp.js
@@ -1,5 +1,5 @@
-import {StyleSheet, View, TextInput, SafeAreaView} from 'react-native';
-import React, {useState} from 'react';
+import {StyleSheet, View, TextInput, SafeAreaView, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import CustomButton from '../shared/components/CustomButton';
 import {AuthContext} from '../context/AuthContext';
 import {useContext} from 'react';
@@ -7,49 +7,96 @@ import {http} from '../shared/lib';
 import {BACKEND_CLIENT_ID} from '../shared/constants/env';
 
 export default function VerifyOtp({route}) {
-  const {userid} = route.params;
+  const {userid, is_email_verified, is_phone_verified} = route.params;
   const {isVerified} = useContext(AuthContext);
   const [enteredOtp, setEnteredOtp] = useState('');
+  const [enteredEmailOtp, setEnteredEmailOtp] = useState('');
+  const [systemSetting, setSystemSetting] = useState({});
+  const [isError, setIsError] = useState(false);
+  const [serverError, setServerError] = useState('');
 
-  const verifyOtp = async otp => {
+  useEffect(() => {
+    getSystemConfig();
+  }, []);
+
+  const getSystemConfig = async () => {
+    let data = await http.get('system-config/', {
+      headers: {
+        clientid: BACKEND_CLIENT_ID,
+        Authorization: 'Bearer nvmOKbrr1VCudtV21IGlISpDCPdtsj',
+      },
+    });
+
+    // console.log(data);
+    var systemSettings = {};
+    for (const ele of data) {
+      systemSettings[ele.key] = ele.value === 'true' ? true : false;
+    }
+    setSystemSetting({...systemSettings});
+  };
+
+  const verifyOtp = async (phone_otp, email_otp) => {
+    const url = `user/${userid}/verify-otp/`;
+    const config = {
+      headers: {
+        clientid: BACKEND_CLIENT_ID,
+      },
+    };
+    const data = {
+      ...(!!phone_otp && {phone_otp}),
+      ...(!!email_otp && {email_otp}),
+    };
+
+    const resp = await http.post(url, data, config);
+    return resp;
+  };
+
+  const handleButtonClick = async () => {
     try {
-      const url = `user/${userid}/verify-otp/`;
-      const config = {
-        headers: {
-          clientid: BACKEND_CLIENT_ID,
-        },
-      };
-      const data = {
-        phone_otp: otp,
-      };
-
-      const resp = await http.post(url, data, config);
-      return resp;
+      let data = await verifyOtp(enteredOtp, enteredEmailOtp);
+      isVerified(data);
     } catch (error) {
       const msg =
         error?.response?.data?.detail ||
         'Something Went Wrong, We are working on it. Please try after Some time';
-      Alert.alert('ERROR', `${msg}`, [{text: 'OK'}]);
+      setServerError(msg);
+      setIsError(true);
     }
   };
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.header}></View>
+      <View style={styles.header} />
       <View style={styles.formContainer}>
-        <TextInput
-          allowFontScaling={false}
-          style={styles.input}
-          placeholder="Enter Mobile OTP"
-          keyboardType="numeric"
-          onChangeText={otp => setEnteredOtp(otp)}
-        />
+        {!is_phone_verified && systemSetting.phone_verification_required && (
+          <TextInput
+            allowFontScaling={false}
+            style={styles.input}
+            placeholder="Enter Mobile OTP"
+            keyboardType="numeric"
+            onChangeText={otp => setEnteredOtp(otp)}
+          />
+        )}
+        {!is_email_verified && systemSetting.email_verification_required && (
+          <TextInput
+            allowFontScaling={false}
+            style={styles.input}
+            placeholder="Enter Email OTP"
+            keyboardType="numeric"
+            onChangeText={otp => setEnteredEmailOtp(otp)}
+          />
+        )}
         <CustomButton
           btnTitle={'Verify'}
           onpress={async () => {
-            let data = await verifyOtp(enteredOtp);
-            isVerified(data);
+            await handleButtonClick();
           }}
         />
+        {isError && (
+          <Text allowFontScaling={false} style={styles.error}>
+            {serverError}
+          </Text>
+        )}
       </View>
     </SafeAreaView>
   );
@@ -71,5 +118,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#E5E5E5',
     borderRadius: 20,
     padding: 10,
+    marginBottom: 10,
+  },
+  error: {
+    fontSize: 14,
+    color: 'red',
+    marginTop: 5,
+    textAlign: 'center',
   },
 });
