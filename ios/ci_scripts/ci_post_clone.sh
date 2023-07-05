56bc2958227c5941054009362cdd70e311d87c8d
vibhor-ts <vibhor.malik@deliveryhero.in>
2023-07-05 20:15:53 +0530

    IOS Test

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -9,12 +9,12 @@
  set -x
 
  # Install dependencies using Homebrew. This is MUST! Do not delete.
- brew install node cocoapods fastlane
+ brew install node cocoapods
 
  # Install yarn and pods dependencies.
  # If you're using Flutter or Swift 
  # just install pods by "pod install" command 
- ls && cd .. && npm i && cd ios && pod install
+ ls
 
  echo "🎯 Stage: Post-clone is done .... "
 
